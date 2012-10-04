61b2569f4c78fad62bbadb2d1017ac34308354e2
Vishnu Sreedharan Rajeevan <vrajeevan3@factor032.cc.gatech.edu>
2012-10-04 00:46:37 -0400

    more cleanups

--- Makefile
+++
@@ -1,8 +0,0 @@
-# kbuild part of makefile
-obj-m += procfs2.o
-#normal makefile
-	KDIR ?=/usr/include/linux/
-
-default:
-	gcc -C $(KDIR) 
-
--- hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/nethome/vrajeevan3/proj1/hello/hello.ko := ld -r -m elf_x86_64   -o /nethome/vrajeevan3/proj1/hello/hello.ko /nethome/vrajeevan3/proj1/hello/hello.o /nethome/vrajeevan3/proj1/hello/hello.mod.o
